1d7946c1db9c9a33354c07047358f5ed422d10a9
godmonera <borwonpak_du@cmu.ac.th>
2023-01-10 15:19:25 +0700

    finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(long long int &);
+
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(long long int &){
+    fibonacci(x) = fibonacci(x-1) + fibonacci(x-2) ;
 }
