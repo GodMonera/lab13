1a387a9cd0a06c6bf56153a85695cff74c2f198f
godmonera <borwonpak_du@cmu.ac.th>
2023-01-10 15:57:49 +0700

    finished

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename Z >
+void mySwap(Z &,Z &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,10 @@ int main(){
 	return 0;
 }
 
+template <typename Z >
+void mySwap(Z &x,Z &y){
+	Z z;
+	z = x;
+	x = y;
+	y = z;
+}
